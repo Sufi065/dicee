c8ccd5c198fa9c3bbe75c0b78609acc9e113f422
Sufi065 <132118766+Sufi065@users.noreply.github.com>
2023-09-25 12:16:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var randomnumber1=Math.floor(Math.random() *6)+1;
+
+var randomdice1="dice"+randomnumber1+".png";
+
+var randomImageSourcee="/images/"+randomdice1;
+
+var image1=document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomImageSourcee);
+
+var randomnumber2=Math.floor(Math.random() *6)+1;
+
+var randomdice2="/images/dice"+randomnumber2+".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomdice2);
+
+if(randomnumber1>randomnumber2) {
+    document.querySelector("h1").innerHTML="🏁Plyar 1 Win"
+}
+else if(randomnumber1<randomnumber2){
+    document.querySelector("h1").innerHTML="Plyar 2 Win🏁"
+
+}
+else {
+    document.querySelector("h1").innerHTML="🏁Draw🏁"
+}
+
